9def7d72edc5b611eaf954729c1fabafdcbd9dcb
Manvendra singh kushwah <kushwah.1@iitj.ac.in>
2017-12-27 21:15:13 +0530

    Update Vectar4 ChangusFinalBattle.cpp

--- Vectar4 ChangusFinalBattle.cpp
+++ Vectar4 ChangusFinalBattle.cpp
@@ -1,1 +1,29 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        unsigned long long int n,x;
+        cin>>n;
+        x=(sqrt(n));
+        if(n==x*x){
+            cout<<2*x-1<<"\n";
+        }
+        else{
+            if(n<=x*x+x){
+                cout<<2*x<<"\n";
+            }
+            else{
+                cout<<2*x+1<<"\n";
+            }
+        }
+
+
+    }
+
+
+
+
+return 0;}
